619e5a8d51b5b0afbaf17fcae63176c8e6ada6fe
Jailynn Philander <53036125+JP-Kai@users.noreply.github.com>
2024-10-14 00:03:14 +0200

    Added Constant Colours.js

---
+++ constants/Colours.js
@@ -0,0 +1,10 @@
+// colours.js
+const colours = {
+  darkBlue: "#1f2c8f",
+  paleBlue: "#aac3e8",
+  lightPink: "#f5cdce",
+  beige: "#ccbe89",
+  blushPink: "#de8c8c",
+};
+
+export default colours;
