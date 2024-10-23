9f3aa9b0f045cd264917a9d41288673ae08551c9
Aahdeal <a.asmal101@gmail.com>
2024-10-23 14:29:45 +0200

    remove header from pages

--- app/Navigation.js
+++ app/Navigation.js
@@ -14,7 +14,7 @@ const Stack = createStackNavigator();
 
 function AppNavigator() {
   return (
-    <Stack.Navigator initialRouteName="Login">
+    <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
       <Stack.Screen name="Login" component={LoginScreen} />
       <Stack.Screen name="Register" component={RegisterScreen} />
       <Stack.Screen name="Home" component={HomeScreen} />
