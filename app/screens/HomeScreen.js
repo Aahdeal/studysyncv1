d743d183b1f02b9e3e3fd234d924e7ebcaa5f759
Aahdeal <a.asmal101@gmail.com>
2024-10-28 01:58:13 +0200

    display events and tasks

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -6,48 +6,170 @@ import NavBar from "../../components/NavBar";
 import { ref, get } from "firebase/database";
 import { database } from '../firebase'; // Ensure this points to your Firebase config
 
-const getRandomMotivationalMessage = async () => {
-  try {
-    const randomIndex = Math.floor(Math.random() * 39);
-    const snapshot = await get(ref(database, `motivationalMessage/${randomIndex}`));
-    const messageData = snapshot.val();
-    return messageData;
-  } catch (error) {
-    console.error('Error fetching motivational message:', error);
-    return null;
-  }
-};
+
 
 export default function HomeScreen({ navigation, user }) {
+
   const [motivationalMessage, setMotivationalMessage] = useState({ message: '', author: '' });
   //fake data to be replaced with database data
   const [hoursStudied, setHoursStudied] = useState([4, 5, 2, 6, 7, 3, 4]); // Example data for bar chart
   const [progress, setProgress] = useState(0.75); // Example progress (75% of goals met)
-  const [tasksDue, setTasksDue] = useState([
-    { id: '1', title: 'Math Assignment', dueDate: 'Oct 25' },
-    { id: '2', title: 'Science Project', dueDate: 'Oct 27' },
-    { id: '3', title: 'History Essay', dueDate: 'Oct 28' },
-    { id: '4', title: 'History Essay', dueDate: 'Oct 28' },
-    { id: '5', title: 'History Essay', dueDate: 'Oct 28' },
-    // Add more tasks as needed
-  ]);
+  const [eventData, setEventData] = useState([]);
+  const [taskData, setTaskData] = useState([]);
+  /*-------------------------DATA ACCESSOR METHODS---------------------- */
 
-  const renderTaskItem = ({ item }) => (
-    <View style={styles.taskItem}>
-      <Text style={styles.taskTitle}>{item.title}</Text>
-      <Text style={styles.taskDueDate}>Due: {item.dueDate}</Text>
-    </View>
-  );
+  let eData = [];
+  let tData = [];
+
+  // ref(database, "users/" + userId + "/calender/events) try with and without the 0
+  const fetchTasks = async () => {
+    let userId = user.uid;
+    const eventsRef = ref(database, `users/${userId}/calendar/tasks`);
+
+    try {
+      // Retrieve data from Firebase
+      const snapshot = await get(eventsRef);
+      
+      if (snapshot.exists()) {
+        snapshot.forEach((childSnapshot) => {
+          const task = childSnapshot.val();  // Get the event data
+          const taskId = childSnapshot.key;
+          console.log("task id: " + childSnapshot.key);  // Get the unique key
+
+          // Push both the key and data into the events array
+          tData.push({task, taskId});
+        });
+        
+        setTaskData(tData);
+        console.log("Fetched tasks:");
+      } else {
+        console.log("No events found for user.");
+      }
+    } catch (error) {
+      console.error("Error fetching tasks:", error);
+    }
+  };
+
+  // ref(database, "users/" + userId + "/calender/events) try with and without the 0
+  const fetchEvents = async () => {
+    let userId = user.uid;
+    const eventsRef = ref(database, `users/${userId}/calendar/events`);
+
+    try {
+      // Retrieve data from Firebase
+      const snapshot = await get(eventsRef);
+      
+      if (snapshot.exists()) {
+        const events = [];
+        snapshot.forEach((childSnapshot) => {
+          const event = childSnapshot.val();  // Get the event data
+          const eventId = childSnapshot.key; // Get the unique key
+
+          // Push both the key and data into the events array
+          eData.push({event, eventId});
+        });
+        
+        setEventData(eData);
+        console.log("Fetched events", eventData);
+      } else {
+        console.log("No events found for user.");
+      }
+    } catch (error) {
+      console.error("Error fetching events:", error);
+    }
+  };
+
+  
+
+  const getRandomMotivationalMessage = async () => {
+    try {
+      const randomIndex = Math.floor(Math.random() * 39);
+      const snapshot = await get(ref(database, `motivationalMessage/${randomIndex}`));
+      const messageData = snapshot.val();
+      return messageData;
+    } catch (error) {
+      console.error('Error fetching motivational message:', error);
+      return null;
+    }
+  };
+
+  //calculate hours studied in the past 7 days
+  const calcHoursStudied = (eventData) => {
+    console.log(eventData[0]['event']);
+    const today = new Date();
+    const sevenDaysAgo = new Date(today);
+    sevenDaysAgo.setDate(today.getDate() - 7);
+      //store all events from 7 days prior to array lastSeven remove all events where type not study
+      var lastSevenDaysEvents = [];
+      for(i = 0; i < eventData.length; i++){
+        let event = eventData[i]['event']['startDate'];
+        console.log(event);
+      }
+      eventData.forEach(event =>{
+        const eventDate = event['event']['startDate'];
+        console.log(eventDate)
+        if(eventDate >= sevenDaysAgo && eventDate <= today && event['event']['type'] === 'Relaxing'){
+          lastSevenDaysEvents.push(event);
+        }
+      });
+    
+      console.log("last 7 days: ", lastSevenDaysEvents);
+      //separate events by day and calculate hours studied for that day by looking at startDate and endDate
+      const hoursByDay = {};
+
+      lastSevenDaysEvents.forEach(event => {
+        const eventDate = new Date(event['event']['startDate']).toDateString();
+        const startDate = new Date(event['event']['startDate']);
+        const endDate = new Date(event['event']['endDate']);
+        
+        // Calculate hours studied for the event
+        const hoursStudied = (endDate - startDate) / (1000 * 60 * 60); // Convert ms to hours
+
+        // Initialize the day's entry if it doesn't exist
+        if (!hoursByDay[eventDate]) {
+          hoursByDay[eventDate] = 0;
+        }
+
+        hoursByDay[eventDate] += hoursStudied;
+        
+
+      });
+      
+
+      const arrayHours = Object.entries(hoursByDay).map(([date, hours]) => ({ date, hours }));
+
+        // Set hours studied (you might want to store it or return it)
+        setHoursStudied(arrayHours);
+
+  };
 
   useEffect(() => {
-    const fetchMessage = async () => {
+    const fetchData = async () => {
       const messageData = await getRandomMotivationalMessage();
       if (messageData) {
         setMotivationalMessage(messageData);
       }
+      fetchEvents();
+      fetchTasks();
     };
-    fetchMessage();
+    fetchData();
+    //calcHoursStudied(eventData);
+    
   }, []);
+
+  const renderTaskItem = ({ item }) => (
+    <View style={styles.taskItem}>
+      <Text style={styles.taskTitle}>{item['task']['title']}</Text>
+      <Text style={styles.taskDueDate}>Due: {item['task']['dueDate']}</Text>
+    </View>
+  );
+
+  const renderEventItem = ({ item }) => (
+    <View style={styles.taskItem}>
+      <Text style={styles.taskTitle}>{item['event']['title']}</Text>
+      <Text style={styles.taskDueDate}>Type: {item['event']['type']}</Text>
+    </View>
+  );
 
   return (
     <View>
@@ -134,9 +256,9 @@ export default function HomeScreen({ navigation, user }) {
         <ScrollView style={styles.listView}
         nestedScrollEnabled = {true}>
           <FlatList
-            data={tasksDue}
+            data={taskData}
             renderItem={renderTaskItem}
-            keyExtractor={(item) => item.id}
+            keyExtractor={(item) => item['taskId']}
           />
         </ScrollView>
       </View>
@@ -146,9 +268,9 @@ export default function HomeScreen({ navigation, user }) {
         <ScrollView style={styles.listView}
         nestedScrollEnabled = {true}>
           <FlatList
-            data={tasksDue}
-            renderItem={renderTaskItem}
-            keyExtractor={(item) => item.id}
+            data={eventData}
+            renderItem={renderEventItem}
+            keyExtractor={(item)=> item['eventId']}
           />
         </ScrollView>
       </View>
